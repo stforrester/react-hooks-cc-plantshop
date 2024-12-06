2def84fceac730a94cc8d30cd434aae9dd21129f
Stuart Forrester <stforrester@gmail.com>
2024-12-06 08:04:48 -0600

    fulfills deliverable #3

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,22 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 function PlantCard({
   src="https://via.placeholder.com/400",
   name="plant name",
   price="plant price" 
-  }) {
-  return (
-    <li className="card" data-testid="plant-item">
-      <img src={src} alt={"plant name"} />
-      <h4>{name}</h4>
-      <p>Price: ${price}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
-      ) : (
-        <button>Out of Stock</button>
-      )}
-    </li>
-  );
+  })
+  { 
+    const [stock, setStock] = useState(true)
+
+    const handleClick = () => {
+      setStock(!stock);
+    }
+
+    return (
+      <li className="card" data-testid="plant-item">
+        <img src={src} alt={"plant name"} />
+        <h4>{name}</h4>
+        <p>Price: ${price}</p>
+        <span>
+          {stock ? (
+            <button className="primary" onClick={handleClick} >In Stock</button>
+          ) : (
+            <button>Out of Stock</button>
+          )}
+          {!stock ? (
+            <>
+              <div></div>
+              <button className="primary" onClick={handleClick} >Re-stock</button>
+            </>
+          ) : (null)}
+        </span>
+      </li>
+    );
 }
 
 export default PlantCard;
